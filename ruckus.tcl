d6bcecf9f2a69bde9847c58af929567a67ee00a0
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-21 08:24:14 -0800

    Update ruckus.tcl

--- ruckus.tcl
+++ ruckus.tcl
@@ -3,14 +3,14 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
-   if { [SubmoduleCheck {ruckus} {1.7.5} ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}   {1.9.8} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {ruckus} {2.0.1}  ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.0.0} ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
-   puts "Ignoring the submodule locks in atlas-rd53-fw-lib/ruckus.tcl"
+   puts "Ignoring the submodule locks in axi-pcie-core/ruckus.tcl"
    puts "*********************************************************\n\n"
-}
+}   
 
 # Get the family type
 set family [getFpgaFamily]
