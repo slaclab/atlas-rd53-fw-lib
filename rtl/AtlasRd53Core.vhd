d8aa4e3a62f06e039a290537dd5bf27fa328c90b
Larry Ruckman <ruckman@slac.stanford.edu>
2020-07-27 14:53:40 -0700

    adding EN_RX_G support

--- rtl/AtlasRd53Core.vhd
+++ rtl/AtlasRd53Core.vhd
@@ -32,6 +32,7 @@ entity AtlasRd53Core is
       AXIS_CONFIG_G : AxiStreamConfigType;
       VALID_THOLD_G : positive              := 128;  -- Hold until enough to burst into the interleaving MUX
       SIMULATION_G  : boolean               := false;
+      EN_RX_G       : boolean               := true;
       RX_MAPPING_G  : Slv2Array(3 downto 0) := (0 => "00", 1 => "01", 2 => "10", 3 => "11");  -- Set the default RX PHY lane mapping
       XIL_DEVICE_G  : string                := "7SERIES";
       SYNTH_MODE_G  : string                := "xpm");
@@ -54,19 +55,19 @@ entity AtlasRd53Core is
       -- Streaming Data Interface (axisClk domain)
       axisClk         : in  sl;
       axisRst         : in  sl;
-      mDataMaster     : out AxiStreamMasterType;
-      mDataSlave      : in  AxiStreamSlaveType;
       sConfigMaster   : in  AxiStreamMasterType;
       sConfigSlave    : out AxiStreamSlaveType;
-      mConfigMaster   : out AxiStreamMasterType;
+      mConfigMaster   : out AxiStreamMasterType   := AXI_STREAM_MASTER_INIT_C;
       mConfigSlave    : in  AxiStreamSlaveType;
+      mDataMaster     : out AxiStreamMasterType   := AXI_STREAM_MASTER_INIT_C;
+      mDataSlave      : in  AxiStreamSlaveType;
       -- Timing/Trigger Interface
       clk160MHz       : in  sl;
       rst160MHz       : in  sl;
       -- Deserialization Interface
       serDesData      : in  Slv8Array(3 downto 0);
-      dlyLoad         : out slv(3 downto 0);
-      dlyCfg          : out Slv9Array(3 downto 0);
+      dlyLoad         : out slv(3 downto 0)       := (others => '0');
+      dlyCfg          : out Slv9Array(3 downto 0) := (others => (others => '0'));
       -- RD53 ASIC Serial Ports
       dPortCmdP       : out sl;
       dPortCmdN       : out sl);
@@ -82,48 +83,48 @@ architecture mapping of AtlasRd53Core is
          tDestBits => 0,
          tUserBits => 2);
 
-   signal dataMaster : AxiStreamMasterType;
-   signal dataCtrl   : AxiStreamCtrlType;
+   signal dataMaster : AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
+   signal dataCtrl   : AxiStreamCtrlType   := AXI_STREAM_CTRL_UNUSED_C;
 
-   signal configMaster : AxiStreamMasterType;
-   signal configCtrl   : AxiStreamCtrlType;
+   signal configMaster : AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
+   signal configCtrl   : AxiStreamCtrlType   := AXI_STREAM_CTRL_UNUSED_C;
 
-   signal txDataMaster : AxiStreamMasterType;
-   signal txDataSlave  : AxiStreamSlaveType;
+   signal txDataMaster : AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
+   signal txDataSlave  : AxiStreamSlaveType  := AXI_STREAM_SLAVE_FORCE_C;
 
-   signal batcherMaster : AxiStreamMasterType;
-   signal batcherSlave  : AxiStreamSlaveType;
+   signal batcherMaster : AxiStreamMasterType := AXI_STREAM_MASTER_INIT_C;
+   signal batcherSlave  : AxiStreamSlaveType  := AXI_STREAM_SLAVE_FORCE_C;
 
-   signal autoReadReg  : Slv32Array(3 downto 0);
-   signal enable       : slv(3 downto 0);
-   signal linkUp       : slv(3 downto 0);
-   signal hdrErrDet    : slv(3 downto 0);
-   signal bitSlip      : slv(3 downto 0);
-   signal selectRate   : slv(1 downto 0);
-   signal rxPhyXbar    : Slv2Array(3 downto 0);
-   signal chBond       : sl;
-   signal debugStream  : sl;
-   signal invData      : slv(3 downto 0);
-   signal invCmd       : sl;
-   signal dlyCmd       : sl;
-   signal batchSize    : slv(15 downto 0);
-   signal timerConfig  : slv(15 downto 0);
-   signal wrdSent      : sl;
-   signal singleHdrDet : sl;
-   signal doubleHdrDet : sl;
-   signal singleHitDet : sl;
-   signal doubleHitDet : sl;
-   signal cmdBusy      : sl;
+   signal autoReadReg  : Slv32Array(3 downto 0) := (others => (others => '0'));
+   signal enable       : slv(3 downto 0)        := (others => '0');
+   signal linkUp       : slv(3 downto 0)        := (others => '0');
+   signal hdrErrDet    : slv(3 downto 0)        := (others => '0');
+   signal bitSlip      : slv(3 downto 0)        := (others => '0');
+   signal selectRate   : slv(1 downto 0)        := (others => '0');
+   signal rxPhyXbar    : Slv2Array(3 downto 0)  := (others => (others => '0'));
+   signal chBond       : sl                     := '0';
+   signal debugStream  : sl                     := '0';
+   signal invData      : slv(3 downto 0)        := (others => '0');
+   signal invCmd       : sl                     := '0';
+   signal dlyCmd       : sl                     := '0';
+   signal batchSize    : slv(15 downto 0)       := (others => '0');
+   signal timerConfig  : slv(15 downto 0)       := (others => '0');
+   signal wrdSent      : sl                     := '0';
+   signal singleHdrDet : sl                     := '0';
+   signal doubleHdrDet : sl                     := '0';
+   signal singleHitDet : sl                     := '0';
+   signal doubleHitDet : sl                     := '0';
+   signal cmdBusy      : sl                     := '0';
 
-   signal localRst    : sl;
-   signal localReset  : sl;
-   signal reset160MHz : sl;
+   signal localRst    : sl := '1';
+   signal localReset  : sl := '1';
+   signal reset160MHz : sl := '1';
 
-   signal dlyConfig     : Slv9Array(3 downto 0);
-   signal enUsrDlyCfg   : sl;
-   signal usrDlyCfg     : Slv9Array(3 downto 0);
-   signal eyescanCfg    : Slv8Array(3 downto 0);
-   signal lockingCntCfg : slv(23 downto 0);
+   signal dlyConfig     : Slv9Array(3 downto 0) := (others => (others => '0'));
+   signal enUsrDlyCfg   : sl                    := '0';
+   signal usrDlyCfg     : Slv9Array(3 downto 0) := (others => (others => '0'));
+   signal eyescanCfg    : Slv8Array(3 downto 0) := (others => (others => '0'));
+   signal lockingCntCfg : slv(23 downto 0)      := (others => '0');
 
 begin
 
@@ -147,6 +148,7 @@ begin
       generic map (
          TPD_G        => TPD_G,
          SIMULATION_G => SIMULATION_G,
+         EN_RX_G      => EN_RX_G,
          RX_MAPPING_G => RX_MAPPING_G)
       port map (
          -- Monitoring Interface (clk160MHz domain)
@@ -220,153 +222,157 @@ begin
          cmdOutP         => dPortCmdP,
          cmdOutN         => dPortCmdN);
 
-   ---------------
-   -- RX PHY Layer
-   ---------------
-   U_RxPhyLayer : entity atlas_rd53_fw_lib.AuroraRxChannel
-      generic map (
-         TPD_G         => TPD_G,
-         AXIS_CONFIG_G => INT_AXIS_CONFIG_C,
-         SIMULATION_G  => SIMULATION_G,
-         SYNTH_MODE_G  => SYNTH_MODE_G)
-      port map (
-         -- Deserialization Interface
-         serDesData    => serDesData,
-         dlyLoad       => dlyLoad,
-         dlyCfg        => dlyConfig,
-         enUsrDlyCfg   => enUsrDlyCfg,
-         usrDlyCfg     => usrDlyCfg,
-         eyescanCfg    => eyescanCfg,
-         lockingCntCfg => lockingCntCfg,
-         bitSlip       => bitSlip,
-         hdrErrDet     => hdrErrDet,
-         -- Timing Interface
-         clk160MHz     => clk160MHz,
-         rst160MHz     => reset160MHz,
-         -- Status/Control Interface
-         enable        => enable,
-         selectRate    => selectRate,
-         invData       => invData,
-         linkUp        => linkUp,
-         chBond        => chBond,
-         wrdSent       => wrdSent,
-         singleHdrDet  => singleHdrDet,
-         doubleHdrDet  => doubleHdrDet,
-         singleHitDet  => singleHitDet,
-         doubleHitDet  => doubleHitDet,
-         rxPhyXbar     => rxPhyXbar,
-         debugStream   => debugStream,
-         -- AutoReg and Read back Interface
-         dataMaster    => dataMaster,
-         configMaster  => configMaster,
-         autoReadReg   => autoReadReg);
+   GEN_RX : if EN_RX_G generate
 
-   -----------------------
-   -- Outbound Config FIFO
-   -----------------------
-   U_ConfigFifo : entity surf.AxiStreamFifoV2
-      generic map (
-         -- General Configurations
-         TPD_G               => TPD_G,
-         SLAVE_READY_EN_G    => false,
-         VALID_THOLD_G       => 1,
-         -- FIFO configurations
-         SYNTH_MODE_G        => SYNTH_MODE_G,
-         MEMORY_TYPE_G       => "block",
-         GEN_SYNC_FIFO_G     => false,
-         FIFO_ADDR_WIDTH_G   => 9,
-         -- AXI Stream Port Configurations
-         SLAVE_AXI_CONFIG_G  => INT_AXIS_CONFIG_C,
-         MASTER_AXI_CONFIG_G => AXIS_CONFIG_G)
-      port map (
-         -- Slave Port
-         sAxisClk    => clk160MHz,
-         sAxisRst    => reset160MHz,
-         sAxisMaster => configMaster,
-         sAxisCtrl   => configCtrl,
-         -- Master Port
-         mAxisClk    => axisClk,
-         mAxisRst    => axisRst,
-         mAxisMaster => mConfigMaster,
-         mAxisSlave  => mConfigSlave);
+      ---------------
+      -- RX PHY Layer
+      ---------------
+      U_RxPhyLayer : entity atlas_rd53_fw_lib.AuroraRxChannel
+         generic map (
+            TPD_G         => TPD_G,
+            AXIS_CONFIG_G => INT_AXIS_CONFIG_C,
+            SIMULATION_G  => SIMULATION_G,
+            SYNTH_MODE_G  => SYNTH_MODE_G)
+         port map (
+            -- Deserialization Interface
+            serDesData    => serDesData,
+            dlyLoad       => dlyLoad,
+            dlyCfg        => dlyConfig,
+            enUsrDlyCfg   => enUsrDlyCfg,
+            usrDlyCfg     => usrDlyCfg,
+            eyescanCfg    => eyescanCfg,
+            lockingCntCfg => lockingCntCfg,
+            bitSlip       => bitSlip,
+            hdrErrDet     => hdrErrDet,
+            -- Timing Interface
+            clk160MHz     => clk160MHz,
+            rst160MHz     => reset160MHz,
+            -- Status/Control Interface
+            enable        => enable,
+            selectRate    => selectRate,
+            invData       => invData,
+            linkUp        => linkUp,
+            chBond        => chBond,
+            wrdSent       => wrdSent,
+            singleHdrDet  => singleHdrDet,
+            doubleHdrDet  => doubleHdrDet,
+            singleHitDet  => singleHitDet,
+            doubleHitDet  => doubleHitDet,
+            rxPhyXbar     => rxPhyXbar,
+            debugStream   => debugStream,
+            -- AutoReg and Read back Interface
+            dataMaster    => dataMaster,
+            configMaster  => configMaster,
+            autoReadReg   => autoReadReg);
 
-   ---------------------
-   -- Outbound Data FIFO
-   ---------------------
-   U_DataFifo : entity surf.AxiStreamFifoV2
-      generic map (
-         -- General Configurations
-         TPD_G               => TPD_G,
-         SLAVE_READY_EN_G    => false,
-         VALID_THOLD_G       => 1,
-         -- FIFO configurations
-         SYNTH_MODE_G        => SYNTH_MODE_G,
-         MEMORY_TYPE_G       => "block",
-         GEN_SYNC_FIFO_G     => false,
-         FIFO_ADDR_WIDTH_G   => 9,
-         -- AXI Stream Port Configurations
-         SLAVE_AXI_CONFIG_G  => INT_AXIS_CONFIG_C,
-         MASTER_AXI_CONFIG_G => INT_AXIS_CONFIG_C)
-      port map (
-         -- Slave Port
-         sAxisClk    => clk160MHz,
-         sAxisRst    => reset160MHz,
-         sAxisMaster => dataMaster,
-         sAxisCtrl   => dataCtrl,
-         -- Master Port
-         mAxisClk    => axisClk,
-         mAxisRst    => axisRst,
-         mAxisMaster => txDataMaster,
-         mAxisSlave  => txDataSlave);
+      -----------------------
+      -- Outbound Config FIFO
+      -----------------------
+      U_ConfigFifo : entity surf.AxiStreamFifoV2
+         generic map (
+            -- General Configurations
+            TPD_G               => TPD_G,
+            SLAVE_READY_EN_G    => false,
+            VALID_THOLD_G       => 1,
+            -- FIFO configurations
+            SYNTH_MODE_G        => SYNTH_MODE_G,
+            MEMORY_TYPE_G       => "block",
+            GEN_SYNC_FIFO_G     => false,
+            FIFO_ADDR_WIDTH_G   => 9,
+            -- AXI Stream Port Configurations
+            SLAVE_AXI_CONFIG_G  => INT_AXIS_CONFIG_C,
+            MASTER_AXI_CONFIG_G => AXIS_CONFIG_G)
+         port map (
+            -- Slave Port
+            sAxisClk    => clk160MHz,
+            sAxisRst    => reset160MHz,
+            sAxisMaster => configMaster,
+            sAxisCtrl   => configCtrl,
+            -- Master Port
+            mAxisClk    => axisClk,
+            mAxisRst    => axisRst,
+            mAxisMaster => mConfigMaster,
+            mAxisSlave  => mConfigSlave);
 
-   ---------------------------------------------------------
-   -- Batch Multiple 64-bit data words into large AXIS frame
-   ---------------------------------------------------------
-   U_DataBatcher : entity atlas_rd53_fw_lib.AtlasRd53RxDataBatcher
-      generic map (
-         TPD_G         => TPD_G,
-         AXIS_CONFIG_G => INT_AXIS_CONFIG_C)
-      port map (
-         -- Clock and Reset
-         axisClk     => axisClk,
-         axisRst     => axisRst,
-         -- Configuration/Status Interface
-         batchSize   => batchSize,
-         timerConfig => timerConfig,
-         -- AXI Streaming Interface
-         sDataMaster => txDataMaster,
-         sDataSlave  => txDataSlave,
-         mDataMaster => batcherMaster,
-         mDataSlave  => batcherSlave);
+      ---------------------
+      -- Outbound Data FIFO
+      ---------------------
+      U_DataFifo : entity surf.AxiStreamFifoV2
+         generic map (
+            -- General Configurations
+            TPD_G               => TPD_G,
+            SLAVE_READY_EN_G    => false,
+            VALID_THOLD_G       => 1,
+            -- FIFO configurations
+            SYNTH_MODE_G        => SYNTH_MODE_G,
+            MEMORY_TYPE_G       => "block",
+            GEN_SYNC_FIFO_G     => false,
+            FIFO_ADDR_WIDTH_G   => 9,
+            -- AXI Stream Port Configurations
+            SLAVE_AXI_CONFIG_G  => INT_AXIS_CONFIG_C,
+            MASTER_AXI_CONFIG_G => INT_AXIS_CONFIG_C)
+         port map (
+            -- Slave Port
+            sAxisClk    => clk160MHz,
+            sAxisRst    => reset160MHz,
+            sAxisMaster => dataMaster,
+            sAxisCtrl   => dataCtrl,
+            -- Master Port
+            mAxisClk    => axisClk,
+            mAxisRst    => axisRst,
+            mAxisMaster => txDataMaster,
+            mAxisSlave  => txDataSlave);
 
-   --------------------
-   -- Resize/Burst FIFO
-   --------------------
-   Burst_FIFO : entity surf.AxiStreamFifoV2
-      generic map (
-         -- General Configurations
-         TPD_G               => TPD_G,
-         SLAVE_READY_EN_G    => true,
-         VALID_THOLD_G       => VALID_THOLD_G,
-         VALID_BURST_MODE_G  => true,
-         -- FIFO configurations
-         SYNTH_MODE_G        => "xpm",
-         MEMORY_TYPE_G       => "block",
-         GEN_SYNC_FIFO_G     => true,
-         FIFO_ADDR_WIDTH_G   => log2(2*VALID_THOLD_G),
-         -- AXI Stream Port Configurations
-         SLAVE_AXI_CONFIG_G  => INT_AXIS_CONFIG_C,
-         MASTER_AXI_CONFIG_G => AXIS_CONFIG_G)
-      port map (
-         -- Slave Port
-         sAxisClk    => axisClk,
-         sAxisRst    => axisRst,
-         sAxisMaster => batcherMaster,
-         sAxisSlave  => batcherSlave,
-         -- Master Port
-         mAxisClk    => axisClk,
-         mAxisRst    => axisRst,
-         mAxisMaster => mDataMaster,
-         mAxisSlave  => mDataSlave);
+      ---------------------------------------------------------
+      -- Batch Multiple 64-bit data words into large AXIS frame
+      ---------------------------------------------------------
+      U_DataBatcher : entity atlas_rd53_fw_lib.AtlasRd53RxDataBatcher
+         generic map (
+            TPD_G         => TPD_G,
+            AXIS_CONFIG_G => INT_AXIS_CONFIG_C)
+         port map (
+            -- Clock and Reset
+            axisClk     => axisClk,
+            axisRst     => axisRst,
+            -- Configuration/Status Interface
+            batchSize   => batchSize,
+            timerConfig => timerConfig,
+            -- AXI Streaming Interface
+            sDataMaster => txDataMaster,
+            sDataSlave  => txDataSlave,
+            mDataMaster => batcherMaster,
+            mDataSlave  => batcherSlave);
+
+      --------------------
+      -- Resize/Burst FIFO
+      --------------------
+      Burst_FIFO : entity surf.AxiStreamFifoV2
+         generic map (
+            -- General Configurations
+            TPD_G               => TPD_G,
+            SLAVE_READY_EN_G    => true,
+            VALID_THOLD_G       => VALID_THOLD_G,
+            VALID_BURST_MODE_G  => true,
+            -- FIFO configurations
+            SYNTH_MODE_G        => "xpm",
+            MEMORY_TYPE_G       => "block",
+            GEN_SYNC_FIFO_G     => true,
+            FIFO_ADDR_WIDTH_G   => log2(2*VALID_THOLD_G),
+            -- AXI Stream Port Configurations
+            SLAVE_AXI_CONFIG_G  => INT_AXIS_CONFIG_C,
+            MASTER_AXI_CONFIG_G => AXIS_CONFIG_G)
+         port map (
+            -- Slave Port
+            sAxisClk    => axisClk,
+            sAxisRst    => axisRst,
+            sAxisMaster => batcherMaster,
+            sAxisSlave  => batcherSlave,
+            -- Master Port
+            mAxisClk    => axisClk,
+            mAxisRst    => axisRst,
+            mAxisMaster => mDataMaster,
+            mAxisSlave  => mDataSlave);
+
+   end generate;
 
 end mapping;
